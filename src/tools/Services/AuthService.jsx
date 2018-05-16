a75d845e4c7c819d44738a7f843a3a6ab2d89e15
a_tretyakov <artem.tretyakov.91@inbox.ru>
2018-05-16 09:51:38 +0300

    fix

--- src/tools/Services/AuthService.jsx
+++ src/tools/Services/AuthService.jsx
@@ -85,28 +85,6 @@ export default class AuthService {
         return decode(this.getToken());
     }
 
-
-    fetch(url, options) {
-        // performs api calls sending the required authentication headers
-        const headers = {
-            'Accept': 'application/json',
-            'Content-Type': 'application/json'
-        }
-
-        // Setting Authorization header
-        // Authorization: Bearer xxxxxxx.xxxxxxxx.xxxxxx
-        if (this.loggedIn()) {
-            headers['Authorization'] = 'Bearer ' + this.getToken()
-        }
-
-        return fetch(url, {
-            headers,
-            ...options
-        })
-            .then(this._checkStatus)
-            .then(response => response.json())
-    }
-
     _checkStatus(response) {
         // raises an error in case response status is not a success
         if (response.status >= 200 && response.status < 300) { // Success status lies between 200 to 300
