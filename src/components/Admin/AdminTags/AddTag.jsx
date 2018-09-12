7ed25a8a2c7f07519b9b3bc474941c1b64c27c8d
a_tretyakov <artem.tretyakov.91@inbox.ru>
2018-09-12 16:27:15 +0300

    создание и редактирование тегов (draft)

---
+++ src/components/Admin/AdminTags/AddTag.jsx
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+
+import apiUrl from '../../../tools/connection';
+import AuthService from '../../../tools/Services/AuthService';
+
+
+class AddTag extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+    }
+
+}
+
+export default AddTag;
