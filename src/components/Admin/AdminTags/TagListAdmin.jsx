8ae09d583ac1b50fb6a60858a2360e024ef006db
a_tretyakov <artem.tretyakov.91@inbox.ru>
2018-09-17 17:35:21 +0300

    fix

---
+++ src/components/Admin/AdminTags/TagListAdmin.jsx
@@ -0,0 +1,52 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+import apiUrl from '../../../tools/connection';
+
+class TagsListAdmin extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            tags: []
+        };
+
+        this.handleAction = this.handleAction.bind(this);
+    }
+
+    componentDidMount() {
+        axios.get(apiUrl + '/tags')
+          .then(response => {
+            const tags = response.data;    
+            this.setState({ tags });
+          })
+          .catch(function (error) {
+            console.log(error);
+          });
+    }
+
+    handleAction(event) {
+      event.preventDefault();
+      console.log(event.target.id);
+      this.props.onAction(event.target.id);
+    }
+    
+
+    render() {
+        console.log(this.state);
+        
+        return (
+            <div class="column">
+             <div class="card">
+            <div class="card-content">
+              <div class="content">
+                {this.state.tags.map(tag =>
+                    <input className="button is-text tags" type="button" value={tag.title} id={tag.id} onClick={this.handleAction} />
+                )}
+              </div>
+            </div>
+          </div>
+        </div>
+        );
+    }
+}
+
+export default TagsListAdmin;
